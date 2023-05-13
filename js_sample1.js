1423084ece2f06e5ee671ae0cd0982a20068d525
kengo <kengo@kengos-MacBook-Air.local>
2023-05-13 16:47:01 +0900

    1

---
+++ js_sample1.js
@@ -0,0 +1,31 @@
+// 変数をアラートする
+// let num1=5;
+// let num2=8
+// alert(num1+num2);
+
+// クリックしたらクラス名をアラートする
+// $(function(){
+//     $('li').on('click',function(){
+//       alert($(this).attr('class'));
+//     });
+//   });
+
+// リロードして２秒後にアラートする
+//   setTimeout(function() {
+//     alert('2秒経ちました！');
+//   }, 2000);
+
+// triggerクラスをクリックしたらtargetクラスが隠れる
+// $(function() {
+//     $('.trigger').click(function() {
+//       $('.target').hide();
+//     });
+//   })
+
+// カラーピッカーで取得した色を反映させよう
+// $(function(){
+//     $('#color-change').on("change", function(){
+//       let select_color = $(this).val();
+//       $('.title').css('color', select_color);
+//     });
+//   });
