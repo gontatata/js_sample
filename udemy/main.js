3de2c5a4212502aa1c8d773e6ef4f52a21484d3b
kengo <kengo@kengos-Air.lan>
2023-05-15 09:28:54 +0900

    ~2-27

--- udemy/main.js
+++ udemy/main.js
@@ -1,2 +1,25 @@
-let count = 0;
-alert(count);
+let count = 30;
+
+// console.log("'");
+
+// jsではキャメルケースが一般的
+let tomatoCount = 0;
+
+// オブジェクトはキーとバリューがたくさん並んでる
+const coffee = {
+  name: 'mocha',
+  isHot: true,
+  size: 350,
+  nutritions: {
+    calories: 430,
+    sugars: 53,
+  },
+  aaaa: {},
+};
+// オブジェクトは. で出てくる。
+console.log(coffee.size);
+coffee.isHot = false;
+console.log(coffee.isHot);
+// 新しいキーを追加もできる
+coffee.barista = 'kengo';
+console.log(coffee);
